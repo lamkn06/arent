35266b740b920a1766437451f9f618d97ed6a859
lam.khuong <lamkn06@gmail.com>
2024-03-23 15:47:53 +0700

    chore: footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,32 @@
 import Container from '@mui/material/Container';
+import { COLOR_DARK_500, COLOR_LIGHT } from '../../commons/colors';
+import { Grid, Typography } from '@mui/material';
+
+const Item = (props: { text: string }): JSX.Element => {
+  return (
+    <Grid item spacing={10} paddingRight={6}>
+      <Typography>{props.text}</Typography>
+    </Grid>
+  );
+};
 
 const Footer = (): JSX.Element => {
   return (
-    <footer>
+    <footer style={{ backgroundColor: COLOR_DARK_500, color: COLOR_LIGHT }}>
       <Container
         sx={{
           display: 'flex',
           alignItems: 'center',
-          minHeight: 64,
-          background: 'red',
+          height: 128,
         }}
       >
-        aa
+        <Grid container spacing={1}>
+          <Item text={'会員登録'} />
+          <Item text={'運営会社'} />
+          <Item text={'利用規約'} />
+          <Item text={'個人情報の取扱について'} />
+          <Item text={'特定商取引法に基づく表記'} />
+        </Grid>
       </Container>
     </footer>
   );
